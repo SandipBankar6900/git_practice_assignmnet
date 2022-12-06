f8763be829e100303074421403419d2a6243a163
SandipBankar6900 <sb4540994@gmail.com>
2022-12-06 15:13:55 +0530

    problem_on_prime_number_&_palindrome

---
+++ index1.js
@@ -0,0 +1,18 @@
+let number=13;
+
+let count=0;
+
+for(let x=0;x<=number;x++)
+{
+  if(number%x==0)
+     count++;
+   
+  } 
+  if(count==1){
+    console.log("Yes");
+   }
+    else{
+       console.log("No")
+               
+     
+
---
+++ index2.js
@@ -0,0 +1,13 @@
+   let str="aba";
+
+ let rev="";
+for(let x=str.length-1;x>=0;x++)
+ {
+   rev=rev+str[x];
+}
+ if(rev!==str){
+   console.log("Yes");
+  else{
+    console.log("No");
+   }
+
